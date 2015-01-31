ab04cefb3c19f891db3d2d7f990795c0b0416454
MLcraft <pythonprogammer@gmail.com>
2015-01-31 15:48:37 -0800

    Update InclinedPlaneSimulation.py

--- InclinedPlaneSimulation.py
+++ InclinedPlaneSimulation.py
@@ -1,14 +1,18 @@
 from visual import *
 scene.autoscale = False
+scene.range = 4
+scene.center = (5, -2, 0)
 
+ramp = box(pos = vector(0, 0, 0), size = (20, 0.5, 5), color = color.white)
+cart = box(pos = vector(0, 0.55, 0), size = (2, 1, 1), color = color.blue)
 
-ramp = box(pos=vector(0, -0.025, 0), size=(20, 0.05, 0.10), color=color.white)
-cart = box(pos = ramp.pos, size = (0.1, 0.04, 0.06), color = color.blue)
 ramp.rotate(angle=-pi/12, axis=(0, 0, 1))
 cart.rotate(angle=-pi/12, axis=(0, 0, 1))
+
 mcart = 0.80
 vcart = vector(0, 0, 0)
-vvector = arrow(pos = cart.pos,axis= vcart, color=cart.color)
+
+vvector = arrow(pos = cart.pos, axis= vcart * 0.01, color=color.green)
 
 deltat = 0.01
 t = 0
@@ -21,3 +25,4 @@ while True:
    cart.pos = cart.pos + (vcart)*deltat
    vvector.pos = cart.pos
    vvector.axis = vcart
+
